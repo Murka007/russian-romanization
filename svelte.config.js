26fb804d1d1099607a516a759688cea9eee9199d
Murka007 <danilkamuratov123@gmail.com>
2025-10-17 04:13:36 +0400

    cool app

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter(),
-        output: {
-            bundleStrategy: "single",
-        },
         paths: {
-            base: process.env.NODE_ENV === "production" ? "/russian-romanization" : ""
+            base: process.env.NODE_ENV === "production" ? "/russian-romanization" : "",
         }
     }
 }
